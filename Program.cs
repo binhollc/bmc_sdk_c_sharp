f7b07295c58500db855358602c51bfed2d92b45a
Gustavo Armagno <gustavoa@gmail.com>
2024-03-26 22:04:17 -0300

    Write an exit command to the bridge

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define the process using ProcessStartInfo
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "bridge",
+            Arguments = "BinhoSupernova",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true, // This prevents the command window from showing up
+        };
+
+        // Start the process
+        using (var process = Process.Start(processStartInfo))
+        {
+            if (process != null)
+            {
+                // Write a command to the process's standard input
+                string jsonString = "{\"command\":\"exit\"}";
+                process.StandardInput.WriteLine(jsonString);
+                process.StandardInput.Flush();
+                process.StandardInput.Close(); // Signal the end of input
+
+                // Read the output from the process
+                string output = process.StandardOutput.ReadToEnd();
+
+                Console.WriteLine("Output from the process:");
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("Failed to start process.");
+            }
+        }
+    }
+}
